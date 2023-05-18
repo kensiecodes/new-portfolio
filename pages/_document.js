52f7461fd2b4ebece793b1a7d89a40e0bca30aee
Mackensie Jack <kensiejack1@gmail.com>
2023-05-18 15:42:11 -0400

    Add title

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Nanum+Myeongjo&family=Open+Sans&display=swap"
           rel="stylesheet"
@@ -14,7 +14,7 @@ export default function Document() {
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
           integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
           referrerPolicy="no-referrer"
         />
       </Head>
